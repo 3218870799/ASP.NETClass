230496225187707560b906defac19f6770001ec8
TeacherCangNulling <3218870799@qq.com>
2019-01-12 11:55:04 +0800

    精简add

--- BookManagementSystem/BookManagementSystem/student/add.aspx.cs
+++ BookManagementSystem/BookManagementSystem/student/add.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    DataBase db = new DataBase();
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -17,59 +18,21 @@ public partial class _Default : System.Web.UI.Page
 
     protected void submit_Click(object sender, EventArgs e)
     {
-        // 连接数据库
-        string connstr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-        SqlConnection conn = new SqlConnection(connstr);
 
-        //新建command对象
-        SqlCommand cmd = new SqlCommand();
-        cmd.Connection = conn;
-        cmd.CommandText = "INSERT INTO [Student] values(@st_id,@st_name,@st_sex,@st_dept,@st_class,@st_credit,@st_pw)";
-        cmd.CommandType = CommandType.Text;
-
-        SqlParameter para1 = new SqlParameter("@st_id",SqlDbType.Int);
-        
-        string sid= st_id.Text.Trim();
+        string sid = st_id.Text.Trim();
         int id = Convert.ToInt32(sid);
+        string  Name = st_name.Text.Trim();
 
-        para1.Value = id;
-        cmd.Parameters.Add(para1);
-
-        SqlParameter para2 = new SqlParameter("@st_name", SqlDbType.NVarChar,50);
-        para2.Value = st_name.Text.Trim();
-        cmd.Parameters.Add(para2);
-
-        SqlParameter para3 = new SqlParameter("@st_sex", SqlDbType.NVarChar, 50);
-        para3.Value = st_sex.Text.Trim();
-        cmd.Parameters.Add(para3);
-
-        SqlParameter para4 = new SqlParameter("@st_dept", SqlDbType.NVarChar, 50);
-        para4.Value = st_dept.Text.Trim();
-        cmd.Parameters.Add(para4);
-
-        SqlParameter para5 = new SqlParameter("@st_class", SqlDbType.NVarChar, 50);
-        para5.Value = st_class.Text.Trim();
-        cmd.Parameters.Add(para5);
-
-        SqlParameter para6 = new SqlParameter("@st_credit", SqlDbType.Int);
-         
+        string Sex = st_sex.Text.Trim();
+        string dept = st_dept.Text.Trim();
+        string StudentClass = st_class.Text.Trim();
         string s_credit= st_credit.Text.Trim();
         int credit = Convert.ToInt32(s_credit);
-        para6.Value = credit;
-        cmd.Parameters.Add(para6);
-
-        SqlParameter para7 = new SqlParameter("@st_pw", SqlDbType.NVarChar, 50);
-        para7.Value = st_pw.Text.Trim();
-        cmd.Parameters.Add(para7);
-
+        string pw = st_pw.Text.Trim();
+       string sql  = "INSERT INTO [Student] values("+id+",'"+Name+"','"+Sex+"','"+dept+"','"+StudentClass+"',"+credit+",'"+pw+"')";
         try
         {
-
-            conn.Open();
-            Response.Write("1");
-            cmd.ExecuteNonQuery();
-            Response.Write("2");
-            
+            db.ExecuteSQL(sql);            
             Response.Redirect("select.aspx");
             Response.Write("<script>alert('添加成功')</script>");
 
@@ -77,7 +40,6 @@ public partial class _Default : System.Web.UI.Page
         catch {
             Response.Write("发生异常");
         }
-        conn.Close();
     }
 
 }
